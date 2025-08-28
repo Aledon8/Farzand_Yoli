2f7c2950f69f8c266a1fbc9158d71d6aeb51ad59
Aledon <aledon.job@gmail.com>
2025-08-29 00:54:47 +0500

    Update views.py
    remove default functions for register, i changed them to APIView

--- backend/farzand_yoli/account/views.py
+++ backend/farzand_yoli/account/views.py
@@ -1,46 +1,13 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer, UpdateUserSerializer
-import json
-from django.views.decorators.csrf import csrf_exempt
 
-
-@csrf_exempt
-def register(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data.get('username')
-        email = data.get('email')
-        password = data.get('password')
-        if email and password:
-            User.objects.create_user(username=username, email=email, password=password)
-            return JsonResponse({'message': 'User registered successfully'})
-        return JsonResponse({'error': 'Invalid data'}, status=400)
-    return JsonResponse({'error': 'Invalid method'}, status=405)
-
-@csrf_exempt
-def login(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data.get('username')
-        password = data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
-            return JsonResponse({'message': 'Login successful'})
-        return JsonResponse({'error': 'Invalid credentials'}, status=400)
-    return JsonResponse({'error': 'Invalid method'}, status=405)
 
 # Register user
-@csrf_exempt
 class RegisterAPIView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
@@ -54,8 +21,8 @@ class RegisterAPIView(APIView):
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 # Login user
-@csrf_exempt
 class LoginAPIView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
@@ -68,6 +35,7 @@ class LoginAPIView(APIView):
                 "token": token.key
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Update user data
 class UpdateUserAPIView(APIView):
